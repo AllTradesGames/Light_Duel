89a8347cf8df0c4e4bec7e9a1950f92bed4898f3
AllTradesGames <alltradesgames@gmail.com>
2019-08-06 18:20:46 -0600

    Update MoveHead.cs

--- Assets/MoveHead.cs
+++ Assets/MoveHead.cs
@@ -5,19 +5,53 @@ using BeardedManStudios.Forge.Networking.Generated;
 
 public class MoveHead : MovementHeadBehavior
 {
+    private Transform head;
+    private Transform leftHand;
+    private Transform rightHand;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (networkObject.IsOwner)
+        {
+            Destroy(transform.Find("ATVRRemotePlayer").gameObject);
+            head = transform.Find("ATVRPlayerController/OVRCameraRig/TrackingSpace/CenterEyeAnchor");
+            leftHand = transform.Find("ATVRPlayerController/OVRCameraRig/TrackingSpace/LeftHandAnchor");
+            rightHand = transform.Find("ATVRPlayerController/OVRCameraRig/TrackingSpace/RightHandAnchor");
+        }
+        else
+        {
+            Destroy(transform.Find("ATVRPlayerController").gameObject);
+            Destroy(transform.Find("HUD Mount").gameObject);
+            transform.Find("ATVRRemotePlayer").gameObject.SetActive(true);
+            head = transform.Find("ATVRRemotePlayer/CenterEyeAnchor");
+            leftHand = transform.Find("ATVRRemotePlayer/LeftHandAnchor");
+            rightHand = transform.Find("ATVRRemotePlayer/RightHandAnchor");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (networkObject != null)
+        if (networkObject.IsOwner)
         {
-            networkObject.Position = transform.position;
-            networkObject.Rotation = transform.rotation;
+            networkObject.headPosition = head.position;
+            networkObject.headRotation = head.rotation;
+            networkObject.leftPosition = leftHand.position;
+            networkObject.leftRotation = leftHand.rotation;
+            networkObject.rightPosition = rightHand.position;
+            networkObject.rightRotation = rightHand.rotation;
         }
+        else
+        {
+            head.position = networkObject.headPosition;
+            head.rotation = networkObject.headRotation;
+            leftHand.position = networkObject.leftPosition;
+            leftHand.rotation = networkObject.leftRotation;
+            rightHand.position = networkObject.rightPosition;
+            rightHand.rotation = networkObject.rightRotation;
+        }
+
     }
 }
